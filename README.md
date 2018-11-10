479c77215f4ffcaa5aa3e62b3d1328c371e5af15
Caian <caianbene@gmail.com>
2018-11-10 12:54:06 -0200

    Add ex3 main file with README

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # ex3
 Quick wrapper for throwing boost exceptions with stacktrace information
+
+## Introduction
+
+This project is intended to be used with [boost](https://www.boost.org/) to enable transparent, switchable stacktracing when throwing instances of `boost::exception` or derived classes.
+
+## Usage
+
+Please refer to [boost exceptions](https://www.boost.org/doc/libs/1_68_0/libs/exception/doc/boost-exception.html) and [boost stacktrace](https://www.boost.org/doc/libs/1_68_0/doc/html/stacktrace.html) to understand the mechanics of semantic tags and stack traces.
+
+This library provides a custom base class for exceptions that inherits `boost::exception` and `std::exception` as well as providing a constructor with a custom message tied to the `what()` virtual method from `std::exception`.
+
+### Namespace
+
+The `exception_base` class and the `traced` typedef used for stacktracing are contained in the `ex3` namespace.
+
+### Throwing exceptions
+
+Use the `EX3_THROW` macro to throw exception as you would normally do with `BOOST_THROW_EXCEPTION`, including adding error information:
+
+```c++
+#include <ex3/exceptions.hpp>
+
+typedef boost::error_info<struct tag_filename, std::string> filename_info;
+
+EX3_THROW(my_io_exception
+    << filename_info("path/to/the/file")
+);
+```
+
+### Getting information
+
+```c++
+boost::diagnostic_information(exception, verbose);
+```
+
+```c++
+boost::current_exception_diagnostic_information(verbose);
+```
+
+```c++
+boost::get_error_info<your_error_info>(exception);
+```
+
+### Disabling stack tracing
+
+If the stacktrace library happens to be incompatible with the compiler, then it can be disabled by defining `EX3_NO_STACKTRACE`. This will transparently remove includes, typedefs and macros associated with stacktrace generation. **Warning: user code that grabs the stacktrace information will not compile!**
---
+++ exceptions.hpp
@@ -0,0 +1,87 @@
+/*
+ * Copyright (C) 2018 Caian Benedicto <caianbene@gmail.com>
+ *
+ * This file is part of ex3.
+ *
+ * ex3 is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * ex3 is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GNU Make; see the file COPYING.  If not, write to
+ * the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+#include <string>
+
+#include <boost/exception/all.hpp>
+
+#if !defined(EX3_NO_STACKTRACE)
+
+#include <boost/stacktrace.hpp>
+
+/**
+ * @brief Macro for throwing an exception with stacktrace.
+ *
+ */
+#define EX3_THROW(ExeptInstance) \
+    BOOST_THROW_EXCEPTION(ExeptInstance \
+        << ex3::traced(boost::stacktrace::stacktrace()))
+
+#else
+
+/**
+ * @brief Macro for throwing an exception without stacktrace.
+ *
+ */
+#define EX3_THROW(ExeptInstance) \
+    BOOST_THROW_EXCEPTION(ExeptInstance)
+
+#endif
+
+namespace ex3 {
+
+#if !defined(EX3_NO_STACKTRACE)
+
+    /** Tag for stacktrace information included in an exception. */
+    typedef boost::error_info<struct tag_stacktrace, boost::stacktrace::stacktrace> traced;
+
+#endif
+
+    /**
+     * @brief Base class when creating exceptions
+     * compatible with boost and std.
+     *
+     */
+    struct exception_base :
+        virtual std::exception,
+        virtual boost::exception
+    {
+        std::string _message;
+
+        virtual const char* what() const throw()
+        {
+            return _message.c_str();
+        }
+
+        exception_base() :
+            _message()
+        {
+        }
+
+        exception_base(const std::string& message) :
+            _message(message)
+        {
+        }
+
+        virtual ~exception_base() throw()
+        {
+        }
+    };
+}
